fbb66b6cb1f33ac8734f4bcdf8b7d0aebdcf90e5
Jeans <sistemadesignstyle@gmail.com>
2025-08-29 13:05:42 -0500

    primer commit

---
+++ dist/proy_test/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-QWR35RXD.mjs";import"./chunk-QDHL7JCM.mjs";import"./chunk-T7QUTHYJ.mjs";import"./chunk-W6XYX7DX.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
